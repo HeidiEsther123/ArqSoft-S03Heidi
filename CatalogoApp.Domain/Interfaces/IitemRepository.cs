44d77a1b57da7773c0e9286635719570acb0f304
HeidiEsther123 <heidi.pena@tecdesoftware.edu.mx>
2026-05-19 08:37:30 -0600

    CREATE: Domain - Item, IItemRepository

---
+++ CatalogoApp.Domain/Interfaces/IitemRepository.cs
@@ -0,0 +1,17 @@
+﻿using CatalogoApp.Domain.Models;
+
+
+
+namespace CatalogoApp.Domain.Interfaces
+{
+    public interface IItemRepository
+    {
+        List<Item> ObtenerTodos();
+
+        Item? ObtenerPorId(int id);
+
+        void Agregar(Item item);
+
+        void Eliminar(int id);
+    }
+}
